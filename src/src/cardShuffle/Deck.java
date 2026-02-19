c040933849a0589c9f103830c4aa7a01f4722678
kpgalarno <philias1116@gmail.com>
2026-02-19 13:55:51 -0800

    konichiwa jits

--- src/src/cardShuffle/Deck.java
+++ src/src/cardShuffle/Deck.java
@@ -1,5 +1,10 @@
 package cardShuffle;
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+import java.util.function.BiConsumer;
 
 import javax.swing.*;
 
@@ -8,10 +13,33 @@ public class Deck {
     int size;
 
     public Deck() {
+        deckArr = new JList<String>();
+        File file;
+        //file = new File("src/cardShuffle/PNG-cards-1.3");
+        //File[] files = file.listFiles();
         size = 52;
+        try {
+            BufferedReader br = new BufferedReader(
+                    new FileReader("src/cardShuffle/PNG-cards-1.3"));
+        br.close();
+        } catch (Exception ex) {
+            return;
+        }
 
-        deckArr.add();
+
+        if (files != null){
+            for (int i = 0; i < size; i++){
+                files[i].getName();
+                String hi = files[i].getPath();
+                deckArr.add(hi,i);
+            }
+        }
+
 
     }
+
+    public shuffle
+
+
 
 }
