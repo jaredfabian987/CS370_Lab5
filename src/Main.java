9208f52f4236e9ee121b2fff945cca56907164cd
Jared Fabian <jarfabian987@gmail.com>
2026-02-19 15:17:52 -0800

    Start some of the CardPanel class, next steps are too implement the extraction of the cards from the folder

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,11 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-//import java.util.Random;
+import java.util.ArrayList;
+import java.util.HashMap;
 
+// references : https://docs.oracle.com/javase/tutorial/uiswing/components/icon.html
+// https://stackoverflow.com/questions/15182329/how-to-set-icon-to-a-jlabel-from-an-image-from-a-folder
 public class Main extends JFrame {
 
     public Main () {
@@ -28,10 +31,13 @@ public class Main extends JFrame {
                 // where we will call the shuffle function
             }
         });
+        // place the shuffle button at the bottom of the screen
         add(shuffleButton, BorderLayout.SOUTH);
 
+        // set the size of the window
         setSize (1400, 700);
 
+        // location and visibility
         setLocationRelativeTo(null);
 
         setVisible(true);
@@ -39,5 +45,40 @@ public class Main extends JFrame {
 
     public static void main(String[] args) {
         new Main();
+    }
+}
+
+// this is our card panel class and will be in charge of
+// displaying the cards and also shuffling them
+class CardPanel extends JPanel {
+
+    // this array of type string will store the names of all of the cards
+    private ArrayList<String> deck = new ArrayList<>();
+    // we will use a hashMap/lookup table to easier find the cards by name\
+    // it will store the name of the card as well as it's corresponding image
+    private java.util.HashMap<String, ImageIcon> icons = new java.util.HashMap<>();
+
+    public CardPanel () {
+        // may need to change if this green is too bright
+        setBackground(Color.GREEN);
+
+        // create a string array for the different ranks
+        String[] ranks = {"ace","2", "3","4","5","6","7","8", "9", "10", "jack", "queen", "king"};
+
+        // create a string array for the different suits
+        String[] suits = {"clubs","diamonds", "hearts", "spades"};
+
+        // we will use this double for look to make sure that we add every combination of suits
+        for (String rank : ranks){
+            for (String suit: suits){
+                deck.add(rank + "_of_" + suit);
+            } // end of string loop
+        } // end of rank loop
+
+        // next step is to extract the images form the folder
+        // i put up some references near the top that help me do this
+        for (String card : deck) {
+        }
+
     }
 }
