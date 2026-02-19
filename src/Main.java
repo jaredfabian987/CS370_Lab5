76bd73767283e6481dc555b960258bea9834abe1
Jared Fabian <jarfabian987@gmail.com>
2026-02-18 23:45:14 -0800

    Created initial JPanel

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,43 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+//import java.util.Random;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main extends JFrame {
+
+    public Main () {
+
+        // set the title of the window
+        setTitle ("Lab 5: Card Shuffler");
+
+        // set the close behavior
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // user border layout so that we can our object in the middle
+        setLayout(new BorderLayout());
+
+        // create a shuffle button
+
+        JButton shuffleButton = new JButton ("Shuffle");
+
+        // When the user clicks the button, call redraw() on our drawing panel
+        shuffleButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // where we will call the shuffle function
+            }
+        });
+        add(shuffleButton, BorderLayout.SOUTH);
+
+        setSize (1400, 700);
+
+        setLocationRelativeTo(null);
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Main();
     }
 }
